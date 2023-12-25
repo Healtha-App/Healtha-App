61cf36db20bed37ea79c4864c7cea700c34025d5
EsraaMagedd <esraamaged21664@gmail.com>
2023-12-25 16:53:32 +0200

    modified

--- App/healtha/lib/encyclopedias/encyclopedia_types.dart
+++ App/healtha/lib/encyclopedias/encyclopedia_types.dart
@@ -8,98 +8,7 @@ class EncyclopediaTypes extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-      backgroundColor: MyApp.myPurple,
-        elevation: 0,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
       body: Center(
-        child: Column(
-          children: [
-            Stack(
-              clipBehavior: Clip.none,
-              children: [
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.1,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                    color: MyApp.myPurple,
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(15),
-                      bottomRight: Radius.circular(15),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  bottom: -50,
-                  left: MediaQuery.of(context).size.width * 0.05,
-                  right: MediaQuery.of(context).size.width * 0.05,
-                  child: Container(
-                    padding: EdgeInsets.all(20),
-                    width: MediaQuery.of(context).size.width * 0.9,
-                    height: 120,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(15),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Color(0xff7c77d1),
-                          offset: Offset(0.0, 2.0),
-                          blurRadius: 1.0,
-                          spreadRadius: 0.0,
-                        ),
-                      ],
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Dive into medical wisdom!",
-                          style: TextStyle(
-                              fontSize: 22,
-                              fontWeight: FontWeight.bold,
-                              color: MyApp.myPurple),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          "your pocket health encyclopedia",
-                          style: TextStyle(fontSize: 14),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 100,),
-            InkWell(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => EncyclopediaPage('Disease','assets/body-scan-c.png')),
-                );
-              },
-              child: MyContainer('Diseases Encyclopedia','assets/body-scan-c.png'),
-            ),
-            SizedBox(height: 30,),
-            InkWell(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => EncyclopediaPage('Lab Test','assets/flask.png')),
-                );
-              },
-              child: MyContainer('Lab Tests Encyclopedia','assets/flask.png'),
-            ),
-          ],
         child: SingleChildScrollView(
           child: Column(
 
@@ -267,6 +176,7 @@ class MyContainer extends StatelessWidget {
               fontWeight: FontWeight.bold,
             ),
           ),
+
         ],
       ),
     );
