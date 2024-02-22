66bdcd9b4a4dda94a635408b09ce35b37332a5c1
EsraaMagedd <esraamaged21664@gmail.com>
2024-02-22 21:47:39 +0200

    committed

--- App/healtha/lib/main.dart
+++ App/healtha/lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         fontFamily: 'Merriweather',
       ),
-      home: HomeScreen()
+      home: SplashScreen()
     );
 
   }
--- App/healtha/lib/prediction/disease_prediction.dart
+++ App/healtha/lib/prediction/disease_prediction.dart
@@ -17,7 +17,8 @@ class Disease extends StatefulWidget {
   @override
   State<Disease> createState() => _DiseaseState();
 }
-
+String predictionResult = '';
+List<String> selectedSymptomNames = [];
 class _DiseaseState extends State<Disease> {
   static const Color myPurple = Color(0xff7c77d1);
 
@@ -62,30 +63,187 @@ class _DiseaseState extends State<Disease> {
     }
   }
 
-
+// Create a mapping of symptoms to indices (zero-based)
+  Map<String, int> symptomsMapping = {
+    "itching": 0,
+    "skin_rash": 1,
+    "nodal_skin_eruptions": 2,
+    "continuous_sneezing": 3,
+    "shivering": 4,
+    "chills": 5,
+    "joint_pain": 6,
+    "stomach_pain": 7,
+    "acidity": 8,
+    "ulcers_on_tongue": 9,
+    "muscle_wasting": 10,
+    "vomiting": 11,
+    "burning_micturition": 12,
+    "spotting_ urination": 13,
+    "fatigue": 14,
+    "weight_gain": 15,
+    "anxiety": 16,
+    "cold_hands_and_feets": 17,
+    "mood_swings": 18,
+    "weight_loss": 19,
+    "restlessness": 20,
+    "lethargy": 21,
+    "patches_in_throat": 22,
+    "irregular_sugar_level": 23,
+    "cough": 24,
+    "high_fever": 25,
+    "sunken_eyes": 26,
+    "breathlessness": 27,
+    "sweating": 28,
+    "dehydration": 29,
+    "indigestion": 30,
+    "headache": 31,
+    "yellowish_skin": 32,
+    "dark_urine": 33,
+    "nausea": 34,
+    "loss_of_appetite": 35,
+    "pain_behind_the_eyes": 36,
+    "back_pain": 37,
+    "constipation": 38,
+    "abdominal_pain": 39,
+    "diarrhoea": 40,
+    "mild_fever": 41,
+    "yellow_urine": 42,
+    "yellowing_of_eyes": 43,
+    "acute_liver_failure": 44,
+    "fluid_overload": 45,
+    "swelling_of_stomach": 46,
+    "swelled_lymph_nodes": 47,
+    "malaise": 48,
+    "blurred_and_distorted_vision": 49,
+    "phlegm": 50,
+    "throat_irritation": 51,
+    "redness_of_eyes": 52,
+    "sinus_pressure": 53,
+    "runny_nose": 54,
+    "congestion": 55,
+    "chest_pain": 56,
+    "weakness_in_limbs": 57,
+    "fast_heart_rate": 58,
+    "pain_during_bowel_movements": 59,
+    "pain_in_anal_region": 60,
+    "bloody_stool": 61,
+    "irritation_in_anus": 62,
+    "neck_pain": 63,
+    "dizziness": 64,
+    "cramps": 65,
+    "bruising": 66,
+    "obesity": 67,
+    "swollen_legs": 68,
+    "swollen_blood_vessels": 69,
+    "puffy_face_and_eyes": 70,
+    "enlarged_thyroid": 71,
+    "brittle_nails": 72,
+    "swollen_extremities": 73,
+    "excessive_hunger": 74,
+    "extra_marital_contacts": 75,
+    "drying_and_tingling_lips": 76,
+    "slurred_speech": 77,
+    "knee_pain": 78,
+    "hip_joint_pain": 79,
+    "muscle_weakness": 80,
+    "stiff_neck": 81,
+    "swelling_joints": 82,
+    "movement_stiffness": 83,
+    "spinning_movements": 84,
+    "loss_of_balance": 85,
+    "unsteadiness": 86,
+    "weakness_of_one_body_side": 87,
+    "loss_of_smell": 88,
+    "bladder_discomfort": 89,
+    "foul_smell_of_urine": 90,
+    "continuous_feel_of_urine": 91,
+    "passage_of_gases": 92,
+    "internal_itching": 93,
+    "toxic_look_(typhos)": 94,
+    "depression": 95,
+    "irritability": 96,
+    "muscle_pain": 97,
+    "altered_sensorium ": 98,
+    "red_spots_over_body": 99,
+    "belly_pain": 100,
+    "abnormal_menstruation": 101,
+    "dischromic_patches": 102,
+    "watering_from_eyes": 103,
+    "increased_appetite": 104,
+    "polyuria": 105,
+    "family_history": 106,
+    "mucoid_sputum": 107,
+    "rusty_sputum": 108,
+    "lack_of_concentration": 109,
+    "visual_disturbances": 110,
+    "receiving_blood_transfusion": 111,
+    "receiving_unsterile_injections": 112,
+    "coma": 113,
+    "stomach_bleeding": 114,
+    "distention_of_abdomen": 115,
+    "history_of_alcohol_consumption": 116,
+    "fluid_overload": 117,
+    "blood_in_sputum": 118,
+    "prominent_veins_on_calf": 119,
+    "palpitations": 120,
+    "painful_walking": 121,
+    "Pus_filled_pimples": 122,
+    "blackheads": 123,
+    "scurring": 124,
+    "skin_peeling": 125,
+    "Silver_like_dusting": 126,
+    "small_dents_in_nails": 127,
+    "inflammatory_nails": 128,
+    "blister": 129,
+    "red_sore_around_nose": 130,
+    "yellow_crust_ooze": 131,
+  };
 
 
   Future<void> _predictDisease(BuildContext context) async {
     try {
+      List<int> symptomsInput = List<int>.filled(132, 0);
+
+      for (int i = 0; i < symptomsFromServer.length; i++) {
+        if (_isSymptomSelected(symptomsFromServer[i])) {
+          symptomsInput[i] = 1;
+        }
+      }
+
+      // Print the selected symptoms for testing
+      print('Selected symptoms: $symptomsInput');
+
       final response = await http.post(
         Uri.parse('http://192.168.56.1:5000/predict'),
         headers: {"Content-Type": "application/json"},
-        body: jsonEncode({'symptoms': selectedSymptoms}),
+        body: jsonEncode({'symptoms': symptomsInput}),
       );
+
       // Print the entire response for debugging
       print('Response: ${response.body}');
 
-      final dynamic jsonResponse = json.decode(response.body);
+      if (response.statusCode == 200) {
+        final dynamic jsonResponse = json.decode(response.body);
 
-      if (jsonResponse is String) {
-        setState(() {
-          predictedDisease = jsonResponse;
-        });
+        if (jsonResponse is Map<String, dynamic> &&
+            jsonResponse.containsKey('prediction')) {
+          final dynamic predictionData = jsonResponse['prediction'];
 
-        // Display predicted disease in the stream message
-        showAfterAnimation = true;
+          if (predictionData is String) {
+            setState(() {
+              predictedDisease = predictionData;
+            });
+
+            // Display predicted disease in the stream message
+            showAfterAnimation = true;
+          } else {
+            print('Invalid prediction format from server.');
+          }
+        } else {
+          print('Invalid response format from server. "prediction" key not found.');
+        }
       } else {
-        print('Invalid response format from server.');
+        print('Failed to predict disease. Status code: ${response.statusCode}');
       }
     } catch (error) {
       print('Error: $error');
@@ -100,10 +258,15 @@ class _DiseaseState extends State<Disease> {
     setState(() {
       if (selectedSymptoms.contains(symptom)) {
         selectedSymptoms.remove(symptom);
+        selectedSymptomNames.remove(symptomsMapping.keys.elementAt(symptomsMapping.values.toList().indexOf(symptomsMapping[symptom]!)));
       } else {
         selectedSymptoms.add(symptom);
+        selectedSymptomNames.add(symptomsMapping.keys.elementAt(symptomsMapping.values.toList().indexOf(symptomsMapping[symptom]! )));
       }
     });
+  }
+  void printSelectedSymptoms() {
+    print('Selected Symptoms: $selectedSymptomNames');
   }
 
   @override
@@ -286,37 +449,38 @@ class _DiseaseState extends State<Disease> {
 
             if (_isEnabled == false)
               Column(
-                children: [
-                  if (showAfterAnimation)
-                    AnimatedTextKit(
-                      animatedTexts: [
-                        TypewriterAnimatedText(
-                          """ The predicted disease is : $predictedDisease
+                  children: [
+                    if (showAfterAnimation)
+                      AnimatedTextKit(
+                        animatedTexts: [
+                          TypewriterAnimatedText(
+                            """ The predicted disease is : $predictedDisease
 Your health disease report is being generated with care...
-                
+
 We will notify you as soon as it is ready
 
 Thank you for allowing us the time to ensure accuracy!""",
-                          textStyle: TextStyle(
-                            fontSize: 14,
-                            color: Colors.black87,
+                            textStyle: TextStyle(
+                              fontSize: 14,
+                              color: Colors.black87,
+                            ),
+                            speed: Duration(milliseconds: 40),
                           ),
-                          speed: Duration(milliseconds: 40),
-                        ),
-                      ],
-                      isRepeatingAnimation: false,
-                      totalRepeatCount: 1,
-                      displayFullTextOnTap: true,
-                      stopPauseOnTap: true,
-                      repeatForever: false,
-                      onFinished: () {
-                        setState(() {
-                          // Do not set showAfterAnimation to true here
-                        });
-                      },
-                    ),
-                ],
-              ),
+                        ],
+                        isRepeatingAnimation: false,
+                        totalRepeatCount: 1,
+                        displayFullTextOnTap: true,
+                        stopPauseOnTap: true,
+                        repeatForever: false,
+                        onFinished: () {
+                          setState(() {
+                            printSelectedSymptoms(); // Print selected symptoms when animation finishes
+                          });
+                        },
+                      ),
+
+                  ],
+                ),
 
             Container(
               width: double.infinity,
