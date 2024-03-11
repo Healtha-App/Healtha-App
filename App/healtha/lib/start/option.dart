20b87a79bb301dec4362042d73035b395d898289
EsraaMagedd <esraamaged21664@gmail.com>
2024-03-11 16:09:40 +0200

    updated

--- App/healtha/lib/start/option.dart
+++ App/healtha/lib/start/option.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -12,6 +13,7 @@ class option extends StatefulWidget {
 class _AnimatedBackgroundState extends State<option>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
+  late Animation<double> _animation;
 
   @override
   void initState() {
@@ -20,6 +22,7 @@ class _AnimatedBackgroundState extends State<option>
       vsync: this,
       duration: Duration(seconds: 10),
     )..repeat(reverse: true);
+    _animation = Tween<double>(begin: 0, end: 1).animate(_controller);
   }
 
   @override
@@ -28,18 +31,8 @@ class _AnimatedBackgroundState extends State<option>
     super.dispose();
   }
 
-  double getWidth(BuildContext context, double percentage) {
-    return MediaQuery.of(context).size.width * percentage;
-  }
-
-  double getHeight(BuildContext context, double percentage) {
-    return MediaQuery.of(context).size.height * percentage;
-  }
-
   @override
   Widget build(BuildContext context) {
-    Animation<double> _animation = Tween<double>(begin: 0, end: 1).animate(_controller);
-
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
@@ -60,8 +53,8 @@ class _AnimatedBackgroundState extends State<option>
               animation: _animation,
               builder: (context, child) {
                 return Positioned(
-                  top: getHeight(context, _animation.value),
-                  left: getWidth(context, _animation.value),
+                  top: MediaQuery.of(context).size.height * _animation.value,
+                  left: MediaQuery.of(context).size.width * _animation.value,
                   child: Container(
                     width: 100,
                     height: 100,
@@ -77,8 +70,8 @@ class _AnimatedBackgroundState extends State<option>
               animation: _animation,
               builder: (context, child) {
                 return Positioned(
-                  top: getHeight(context, 1 - _animation.value),
-                  left: getWidth(context, 1 - _animation.value),
+                  top: MediaQuery.of(context).size.height * (1 - _animation.value),
+                  left: MediaQuery.of(context).size.width * (1 - _animation.value),
                   child: Container(
                     width: 100,
                     height: 100,
@@ -93,14 +86,18 @@ class _AnimatedBackgroundState extends State<option>
             Padding(
               padding: const EdgeInsets.all(12.0),
               child: Column(
+                //mainAxisAlignment: MainAxisAlignment.center,
+               // crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  SizedBox(height: getHeight(context, 0.07)),
+                  SizedBox(height: 50,),
                   Row(
+                    //mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Text("Step into a world of holistic\nhealth with",style: TextStyle(
                         fontSize: 25,
                         fontWeight: FontWeight.bold,
                       ),),
+
                     ],
                   ),
                   Row(
@@ -116,83 +113,80 @@ class _AnimatedBackgroundState extends State<option>
                         ),
                       ),
                     ],
-                  ),
-                  Spacer(),
-                 // SizedBox(height: getHeight(context, 0.3)),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 50.0),
-                    child: Column(
-                      children: [
-                        Container(
-                          width: getWidth(context, 0.9),
-                          height: getHeight(context, 0.07),
-                          child: ElevatedButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
 
-                                MaterialPageRoute(builder: (context) => EncyclopediaTypes()),
-                              );
-                            },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.transparent,
-                              padding: EdgeInsets.all(16),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(30),
-                                side: BorderSide(
-                                  color: Colors.white, // Change border color here
-                                ),
-                              ),
-                            ),
-                            child: Center(
-                              child: Text(
-                                'Show encyclopedia',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold
-                                ),
-                              ),
+                  ),
+                  SizedBox(height: 300,),
+                  Container(
+                    width: 400,
+                    height: 60,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => EncyclopediaTypes()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.transparent,
+                        padding: EdgeInsets.all(16),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          side: BorderSide(
+                            color: Colors.white, // Change border color here
+                          ), // Remove border
+                        ),
+                      ),
+                      child: SizedBox(
+                        width: 300,
+                        child: Center(
+                          child: Text(
+                            'Show encyclopedia',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold
                             ),
                           ),
                         ),
-                        SizedBox(height: getHeight(context, 0.03)),
-                        Container(
-                          width: getWidth(context, 0.9),
-                          height: getHeight(context, 0.07),
-                          child: ElevatedButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => joinAs()),
-                              );
-                            },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.transparent,
-                              padding: EdgeInsets.all(16),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(30),
-                                side: BorderSide(
-                                  color: Colors.white, // Change border color here
-                                ),
-                              ),
-                            ),
-                            child: Center(
-                              child: Text(
-                                'Start using app',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
+                      ),
                     ),
                   ),
-
+                  SizedBox(height: 30,),
+                  Container(
+                    width: 400,
+                    height: 60,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => joinAs()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.transparent,
+                        padding: EdgeInsets.all(16),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          side: BorderSide(
+                            color: Colors.white, // Change border color here
+                          ), // Remove border
+                        ),
+                      ),
+                      child: SizedBox(
+                        width: 300,
+                        child: Center(
+                          child: Text(
+                            'Start using app',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
               ),
             )
