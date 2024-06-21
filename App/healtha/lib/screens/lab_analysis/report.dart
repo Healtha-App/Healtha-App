21cb7b72a27ececb323d2f82ba47e808df010d56
RahmaK44 <Rhma.khaled20@fcai.usc.edu.eg>
2024-06-21 16:46:15 +0300

    Apply Screen reader on patient report

--- App/healtha/lib/screens/lab_analysis/report.dart
+++ App/healtha/lib/screens/lab_analysis/report.dart
@@ -1,12 +1,15 @@
 import 'dart:convert';
 import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:healtha/screens/generated/l10n.dart';
-import 'package:healtha/variables.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:http/http.dart' as http;
 
+import '../generated/l10n.dart';
+
 class Report extends StatefulWidget {
-  const Report({super.key});
+  const Report({Key? key}) : super(key: key);
 
   @override
   _ReportState createState() => _ReportState();
@@ -16,10 +19,68 @@ class _ReportState extends State<Report> {
   String _translatedReport = '';
   bool _isTranslated = false;
   bool _isTranslating = false;
+  bool _showDelayedMessage = true;
   static const String _apiKey = 'sec_CR4fnBuCT0yYpaeD92AfG1BSihAL9Rq9';
 
-  Future<void> _saveReport(
-      int userId, String filePath, String reportContent) async {
+  final FlutterTts _flutterTts = FlutterTts();
+  bool _isPlaying = false;
+  bool _isVolumeHigh = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _initTTS();
+    _translateReport(); // Initial translation
+    Future.delayed(const Duration(seconds: 10), () {
+      if (mounted) {
+        setState(() {
+          _showDelayedMessage = false;
+        });
+      }
+    });
+  }
+
+  void _initTTS() async {
+    List<dynamic> voices = await _flutterTts.getVoices;
+    List<Map<String, String>> englishVoices = voices
+        .where((voice) => (voice["name"]?.contains("en") ?? false))
+        .map((voice) => Map<String, String>.from(voice))
+        .toList();
+
+    if (englishVoices.isNotEmpty) {
+      await _flutterTts.setVoice(englishVoices.first);
+    }
+
+    // Set the speech rate here
+    await _flutterTts.setSpeechRate(1.0); // 1.0 is normal speed
+  }
+
+  void _speak(String text) async {
+    await _flutterTts.speak(text);
+    setState(() {
+      _isPlaying = true;
+    });
+  }
+
+  void _stop() async {
+    await _flutterTts.stop();
+    setState(() {
+      _isPlaying = false;
+    });
+  }
+
+  void _toggleSound() {
+    if (_isPlaying) {
+      _stop();
+    } else {
+      _speak(_translatedReport);
+    }
+    setState(() {
+      _isVolumeHigh = !_isVolumeHigh;
+    });
+  }
+
+  Future<void> _saveReport(int userId, String filePath, String reportContent) async {
     if (_translatedReport.isEmpty) {
       // If translated report is not available yet, wait for translation to complete
       return;
@@ -32,8 +93,7 @@ class _ReportState extends State<Report> {
 
     try {
       final response = await http.post(
-        Uri.parse(
-            'http://ec2-18-221-98-187.us-east-2.compute.amazonaws.com:4000/api/healtha/reports'),
+        Uri.parse('http://ec2-18-221-98-187.us-east-2.compute.amazonaws.com:4000/api/healtha/reports'),
         headers: {
           'Content-Type': 'application/json',
         },
@@ -66,8 +126,7 @@ class _ReportState extends State<Report> {
       _isTranslating = true;
     });
 
-    String promptWithLanguage =
-        _isTranslated ? _getTranslatedPrompt() : _getOriginalPrompt();
+    String promptWithLanguage = _isTranslated ? _getTranslatedPrompt() : _getOriginalPrompt();
 
     final response = await http.post(
       Uri.parse('https://api.chatpdf.com/v1/chats/message'),
@@ -129,171 +188,229 @@ class _ReportState extends State<Report> {
   }
 
   @override
-  void initState() {
-    super.initState();
-    // Fetch initial report data
-    _translateReport();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final Size screenSize = MediaQuery.of(context).size;
-    final myPurple = Theme.of(context).colorScheme.secondary;
-
     return SafeArea(
-      child: Scaffold(
-        body: Stack(
-          children: [
-            // Background Gradient
-            Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    Color(0xff7c77d1), // Light blue
-                    Color(0xff7c77d1).withOpacity(0.2),
-                    Colors.transparent// Light grey
-                  ],
-                ),
-              ),
-            ),
-            // Abstract Shapes
-            Positioned(
-              top: -screenSize.width * 0.3,
-              right: -screenSize.width * 0.1,
-              child: Container(
-                width: screenSize.width * 0.6,
-                height: screenSize.width * 0.6,
-                decoration: BoxDecoration(
-                  color: Color(0xff7c77d1), // Purple
-                  borderRadius: BorderRadius.circular(screenSize.width * 0.3),
-                ),
-              ),
-            ),
-            Positioned(
-              top: screenSize.height * 0.3,
-              left: -screenSize.width * 0.2,
-              child: Container(
-                width: screenSize.width * 0.4,
-                height: screenSize.width * 0.4,
-                decoration: BoxDecoration(
-                  color: Color(0xff7c77d1), // Purple
-                  borderRadius: BorderRadius.circular(screenSize.width * 0.2),
-                ),
-              ),
-            ),
-            Positioned(
-              bottom: screenSize.height * 0.01,
-              right: screenSize.width * 0.01,
-              child: Container(
-                width: screenSize.width * 0.2,
-                height: screenSize.width * 0.2,
-                decoration: BoxDecoration(
-                  color: AppConfig.myPurple, // Purple
-                  borderRadius: BorderRadius.circular(screenSize.width * 0.1),
-                ),
-              ),
-            ),
-            Positioned.fill(
-              child: BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                child: Container(
-                  color: Colors.white.withOpacity(0.1),
-                ),
-              ),
-            ),
-            Center(
-              child: Container(
+        child: Scaffold(
+        body: LayoutBuilder(
+        builder: (context, constraints) {
+      final bool isLargeScreen = constraints.maxWidth > 600;
+      final double fontSize = isLargeScreen ? 18 : 14;
+      final double padding = isLargeScreen ? 30 : 20;
+      final double buttonWidth = isLargeScreen ? 0.6 : 0.8;
 
-                height: MediaQuery.of(context).size.height * 0.82,
-                width: MediaQuery.of(context).size.width * 0.9,
-
-                decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(0.7),
-                  borderRadius: BorderRadius.circular(15), // Set the radius to make the borders rounded
-
+      return Stack(
+        children: [
+      // Background Gradient
+      Container(
+      decoration: BoxDecoration(
+      gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Color(0xff7c77d1), // Light blue
+          Color(0xff7c77d1).withOpacity(0.2),
+          Colors.transparent // Light grey
+        ],
+      ),
+    ),
+    ),
+    // Abstract Shapes
+    Positioned(
+    top: -constraints.maxWidth * 0.3,
+    right: -constraints.maxWidth * 0.1,
+    child: Container(
+    width: constraints.maxWidth * 0.6,
+    height: constraints.maxWidth * 0.6,
+    decoration: BoxDecoration(
+    color: Color(0xff7c77d1), // Purple
+    borderRadius: BorderRadius.circular(constraints.maxWidth * 0.3),
+    ),
+    ),
+    ),
+    Positioned(
+    top: constraints.maxHeight * 0.3,
+    left: -constraints.maxWidth * 0.2,
+    child: Container(
+    width: constraints.maxWidth * 0.4,
+    height: constraints.maxWidth * 0.4,
+    decoration: BoxDecoration(
+    color: Color(0xff7c77d1), // Purple
+    borderRadius: BorderRadius.circular(constraints.maxWidth * 0.2),
+    ),
+    ),
+    ),
+    Positioned(
+    bottom: constraints.maxHeight * 0.01,
+    right: constraints.maxWidth * 0.01,
+    child: Container(
+    width: constraints.maxWidth * 0.2,
+    height: constraints.maxWidth * 0.2,
+    decoration: BoxDecoration(
+    color: Color(0xff7c77d1), // Purple
+    borderRadius: BorderRadius.circular(constraints.maxWidth * 0.1),
+    ),
+    ),
+    ),
+    Positioned.fill(
+    child: BackdropFilter(
+    filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+    child: Container(
+    color: Colors.white.withOpacity(0.1),
+    ),
+    ),
+    ),
+    Center(
+    child: Container(
+    height: constraints.maxHeight * 0.82,
+    width: constraints.maxWidth * 0.9,
+    decoration: BoxDecoration(
+    color: Colors.white.withOpacity(0.7),
+    borderRadius: BorderRadius.circular(15),
+    ),
+    child: Padding(
+    padding: EdgeInsets.all(padding),
+    child: Column(
+    children: [
+    Container(
+    padding: const EdgeInsets.all(20),
+    child: Text(
+    "Healtha Report",
+    style: TextStyle(
+    fontSize: fontSize,
+    fontWeight: FontWeight.bold,
+    color:Color(0xff7c77d1),
+    ),
+    ),
+    ),
+      const Divider(),
+      Expanded(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(padding),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  _translatedReport,
+                  style: TextStyle(
+                    fontSize: fontSize,
+                  ),
                 ),
-                child: Padding(
-                  padding: EdgeInsets.all(screenSize.width * 0.05),
-                  child: Column(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(20),
-                        child: Text(
-                          "Healtha Report",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: myPurple,
-                          ),
+                const SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                  onPressed: !_isTranslating
+                      ? () {
+                    // Save the report without triggering translation
+                    _saveReport(1, 'file_path', _translatedReport); // Pass report content
+                  }
+                      : null, // Disable button if translation is ongoing
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: Color(0xff7c77d1),
+                    minimumSize: Size(constraints.maxWidth * buttonWidth, 50),
+                  ),
+                  child: Text(
+                    _isTranslated ? "Save" : "حفظ",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: fontSize,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                InkWell(
+                  onTap: () {
+                    if (!_isTranslating) {
+                      _translateReport();
+                    }
+                  },
+                  child: Text(
+                    "Translate this report",
+                    style: TextStyle(
+                      color: Color(0xff7c77d1),
+                      fontSize: fontSize,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+              ],
+            ),
+          ),
+        ),
+      ),
+    ],
+    ),
+    ),
+    ),
+    ),
+        ],
+      );
+        },
+        ),
+          floatingActionButton: Stack(
+            children: [
+              Positioned(
+                top: MediaQuery.of(context).size.height * 0.02, // Adjust position based on screen size
+                left: MediaQuery.of(context).size.width * 0.2, // Adjust position based on screen size
+                child: Visibility(
+                  visible: _showDelayedMessage,
+                  child: AnimatedOpacity(
+                    opacity: _showDelayedMessage ? 1.0 : 0.0,
+                    duration: const Duration(milliseconds: 500),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+                      decoration: BoxDecoration(
+                        color: Colors.black.withOpacity(0.7),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Text(
+                        S.of(context).Hey_nI_am_here_to_read_the_page_for_you,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 13,
+                          fontFamily: S.of(context).Roboto, // Example font family
+                          fontStyle: FontStyle.italic, // Example style
                         ),
                       ),
-                      const Divider(),
-                      Expanded(
-                        child: SingleChildScrollView(
-                          child: Padding(
-                            padding: const EdgeInsets.all(20),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Text(
-                                  _translatedReport ?? '',
-                                  style: const TextStyle(
-                                    fontSize: 14,
-                                  ),
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                ElevatedButton(
-                                  onPressed: !_isTranslating
-                                      ? () {
-                                    // Save the report without triggering translation
-                                    _saveReport(1, 'file_path', _translatedReport); // Pass report content
-                                  }
-                                      : null, // Disable button if translation is ongoing
-                                  style: ElevatedButton.styleFrom(
-                                    foregroundColor: Colors.white,
-                                    backgroundColor: AppConfig.myPurple,
-                                    minimumSize: Size(screenSize.width * 0.8, 50),
-                                  ),
-                                  child: Text(
-                                    _isTranslated ? "Save" : "حفظ",
-                                    style: const TextStyle(
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                ),
-                                const SizedBox(height: 10),
-                                InkWell(
-                                  onTap: () {
-                                    if (!_isTranslating) {
-                                      _translateReport();
-                                    }
-                                  },
-                                  child: Text(
-                                    "Translate this report",
-                                    style: const TextStyle(
-                                      color: Color(0xff7c77d1),
-                                      decoration: TextDecoration.underline,
-                                    ),
-                                  ),
-                                ),
-                                const SizedBox(height: 10),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+
+              Positioned(
+                top: MediaQuery.of(context).size.height * 0.02, // Adjust position based on screen size
+                right: 0,
+                child: InkWell(
+                  onTap: () {
+                    _toggleSound();
+                  },
+                  borderRadius: BorderRadius.circular(28.0),
+                  child: Container(
+                    width: 56,
+                    height: 56,
+                    decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.7),
+                      shape: BoxShape.circle,
+                    ),
+                    child: Center(
+                      child: Icon(
+                        _isVolumeHigh
+                            ? FontAwesomeIcons.volumeHigh
+                            : FontAwesomeIcons.volumeUp,
+                        size: 20,
+                        color: _isVolumeHigh ? Colors.white : const Color(0xff7c77d1),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
     );
   }
 }
+
